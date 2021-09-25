bb9d98c4d93d7702905f817c7468ac9285a23bc8
rgomezwap <rgomezwap@yahoo.es>
2021-09-25 16:41:20 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 scene.setBackgroundImage(assets.image`background1`)
 tiles.setTilemap(tilemap`level1`)
-let mySprite = sprites.create(assets.image`plane0`, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`starship1`, SpriteKind.Player)
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 let mySprite2 = sprites.create(assets.image`boca1`, SpriteKind.Food)
@@ -10,8 +10,8 @@ for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
 }
 let mySprite3 = sprites.create(assets.image`barriga1`, SpriteKind.Food)
 tiles.placeOnRandomTile(mySprite3, assets.tile`myTile0`)
-for (let value of tiles.getTilesByType(assets.tile`myTile0`)) {
-    tiles.setTileAt(value, assets.tile`transparency16`)
+for (let value2 of tiles.getTilesByType(assets.tile`myTile0`)) {
+    tiles.setTileAt(value2, assets.tile`transparency16`)
 }
 game.onUpdate(function () {
     scene.centerCameraAt(mySprite.x + 40, mySprite.y)
